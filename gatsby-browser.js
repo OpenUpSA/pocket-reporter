c48ad0c6fd29948890ddba848af92e77f4b6fa6d
Schalk Venter <venterschalk@gmail.com>
2018-12-26 18:29:46 +0200

    Fixes issue with Redux Provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import store from "./src/redux/store";
+import { Provider } from 'react-redux';
 
 
 export default ({ element }) => <Provider {...{ store }}>{element}</Provider>
