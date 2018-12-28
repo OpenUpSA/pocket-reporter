1c2d5ad6878e0e3863232afd18f5357115663afc
AliciadW <alicia.dewet@living-stratus.com>
2018-12-28 17:48:43 +0200

    More work on Questions component

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -9,22 +9,64 @@ const questionData = [
   {
     question: 'Who is the person or organisation filing the action? Names, ages, addresses, other personal information.',
     hint: 'Write your answer here.'
-  },{
+  },
+  {
     question: 'Could this suit lead to a landmark action? Is it a precedent?',
     hint: 'Write your answer here.'
-  },{
+  },
+  {
     question: 'Is there a possibility of an out-of-court settlement?',
     hint: 'Write your answer here.'
-  },{
+  },
+  {
     question: 'What is the significance of the action and the effect on others?',
     hint: 'Write your answer here.'
-  },{
+  },
+  {
     question: 'What are the names of the lawyers for both sides? (Check spelling!)',
     hint: 'Write your answer here.'
-  },{
+  },
+  {
     question: 'What is the date and who is the presiding judge for the trial or hearing?',
     hint: 'Write your answer here.'
-  },{
+  },
+  {
+    question: 'What is the background of the plaintiff or petitioner: the person filing the action?',
+    hint: 'Write your answer here.'
+  },
+  {
+    question: 'What is the background of the plaintiff or petitioner: the person filing the action?',
+    hint: 'Write your answer here.'
+  },
+  {
+    question: 'What is the background of the plaintiff or petitioner: the person filing the action?',
+    hint: 'Write your answer here.'
+  },
+  {
+    question: 'What is the background of the plaintiff or petitioner: the person filing the action?',
+    hint: 'Write your answer here.'
+  },
+  {
+    question: 'What is the background of the plaintiff or petitioner: the person filing the action?',
+    hint: 'Write your answer here.'
+  },
+  {
+    question: 'What is the background of the plaintiff or petitioner: the person filing the action?',
+    hint: 'Write your answer here.'
+  },
+  {
+    question: 'What is the background of the plaintiff or petitioner: the person filing the action?',
+    hint: 'Write your answer here.'
+  },
+  {
+    question: 'What is the background of the plaintiff or petitioner: the person filing the action?',
+    hint: 'Write your answer here.'
+  },
+  {
+    question: 'What is the background of the plaintiff or petitioner: the person filing the action?',
+    hint: 'Write your answer here.'
+  },
+  {
     question: 'What is the background of the plaintiff or petitioner: the person filing the action?',
     hint: 'Write your answer here.'
   },
@@ -32,6 +74,8 @@ const questionData = [
 
 const QuestionWrapper = styled.div`
   font-family: "Roboto", sans-serif;
+  overflow: scroll;
+  height: 100vh;
   @media (min-width: 760px) 
   {
     width: 50%;
@@ -40,9 +84,8 @@ const QuestionWrapper = styled.div`
 `;
 
 const QuestionList = styled.ol`
-  padding: 0 0 0 15px;
-  margin: 0;
-
+  padding: 0 0 0 25px;
+  margin: 10 0;
 `;
 
 const ListItem = styled.li`
