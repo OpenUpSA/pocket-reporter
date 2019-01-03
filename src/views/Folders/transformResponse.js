4ba409c2a963a0bf95d4f685cce22b30de4cbeb5
Schalk Venter <venterschalk@gmail.com>
2019-01-03 08:59:39 +0200

    Adds 'resources' property to data exposed to Folders view

--- src/views/Folders/transformResponse.js
+++ src/views/Folders/transformResponse.js
@@ -1,13 +1,38 @@
 
+const parseItems = ({ questionaires, type }) => {
+  const extractBasedOnType = (item) => {
+    if (item.story_template.post_type === type) {
+      return item.story_template.ID;
+    }
+
+    return null;
+  };
+  
+  const parsed = questionaires.map(extractBasedOnType);                  
+  const result = parsed.filter(value => value !== null);
+  return result;
+}
+
+
 const transformResponse = (rawData) => {
-  const result = JSON.parse(rawData).map((item) => ({
-    icon: item.acf.icon,
-    title: item.title.rendered,
-    storyTemplates: item.acf.questionaires.map(
-      (template) => template.story_template.ID
-    ),
-  }))
+  const parseItem = (item) => {
+    const icon = item.acf.icon;
+    const title = item.title.rendered;
+    const questionaires = item.acf.questionaires;
+    const storyTemplates = parseItems({ questionaires, type: 'story_templates' });
+    const resources = parseItems({ questionaires, type: 'resources' });
     
+    return {
+      icon,
+      title,
+      storyTemplates,
+      resources,
+    };
+  }
+  
+  const data = JSON.parse(rawData);
+  const result = data.map(parseItem);
+  console.log(result);
   return result;
 }
 
