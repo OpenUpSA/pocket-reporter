c30b2af3a5064640e00c708ab1e06719e7e03c2e
Schalk Venter <venterschalk@gmail.com>
2019-01-09 18:53:59 +0200

    Adds `about` page based on GraphQL query

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,64 @@
 import React from 'react';
 import AboutPage from '../views/AboutPage';
+import { graphql } from 'gatsby';
 
-export default () => <AboutPage />
+
+const parseProps = (data) => {
+  const { 
+    html,
+    frontmatter,
+  } = data.allMarkdownRemark.edges[0].node;
+
+  const {
+    tagline,
+    contributors,
+    partners,
+  } = frontmatter;
+
+  return {
+    tagline,
+    html,
+    partners,
+    contributors,
+  };
+}
+
+
+export default ({ data }) => {
+  const props = parseProps(data);
+  return <AboutPage {...props} />
+}
+
+
+export const query = graphql`{
+  allMarkdownRemark (
+    filter: {
+      frontmatter: {
+        type: {
+          eq: "static"
+        }
+        page: {
+          eq: "about"
+        }
+      }
+    }
+  ) {
+    edges {
+      node {
+        html
+        frontmatter {
+          tagline
+          partners {
+            link
+            logo
+            name
+          }
+          contributors {
+            link
+            name
+          }
+        }
+      }
+    }
+  }
+}`
