fbc218fb083653385f3d36c9267b38a07c28eea4
Schalk Venter <venterschalk@gmail.com>
2019-02-23 09:09:26 +0200

    Re-orders components folder

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -1,11 +1,67 @@
 import React from 'react';
+import t from 'prop-types';
 import iconsObject from './iconsObject';
 
 
 const Icon = ({ type }) => {
   const Result = iconsObject[type];
   return <Result />;
-}
+};
 
 
 export default Icon;
+
+
+Icon.propTypes = {
+  type: t.oneOf([
+    'Public',
+    'School',
+    'Poll',
+    'Person',
+    'People',
+    'Notifications',
+    'FreeBreakfast',
+    'FitnessCenter',
+    'ChildFriendly',
+    'Casino',
+    'BusinessCenter',
+    'AirportShuttle',
+    'Wifi',
+    'Wc',
+    'TimeToLeave',
+    'Power',
+    'Cancel',
+    'Train',
+    'Traffic',
+    'StoreMallDirectory',
+    'Restaurant',
+    'Place',
+    'LocalShipping',
+    'LocalPostOffice',
+    'LocalPlay',
+    'LocalPhone',
+    'LocalTaxi',
+    'LocalHospital',
+    'ShoppingCart',
+    'LocalGasStation',
+    'Flight',
+    'WbSunny',
+    'Timelapse',
+    'Image',
+    'FlashOn',
+    'ColorLens',
+    'Audiotrack',
+    'Security',
+    'PhoneIphone',
+    'KeyboardVoice',
+    'Weekend',
+    'Report',
+    'Mail',
+    'HowToVote',
+    'VpnKey',
+    'VolumeUp',
+    'Work',
+    'Grade',
+    'Info',
+  ]).isRequired,
+};
