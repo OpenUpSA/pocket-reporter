19e30ec7257333fe62d662ed20094fecabb298e5
Schalk Venter <venterschalk@gmail.com>
2019-02-26 07:30:39 +0200

    Fixes issue with folders in CMS

--- src/cms/collections/createFolders.js
+++ src/cms/collections/createFolders.js
@@ -1,58 +1,4 @@
-
-
-import { isoToLanguage } from '../../helpers/languageConversions';
-
-const langaugeInstance = (required = true) => [
-  {
-    name: 'folders_title',
-    label: 'Folder Title',
-    widget: 'string',
-    required,
-  },
-  {
-    name: 'questions_link',
-    label: '✏️ Questions Title',
-    widget: 'list',
-    field: {
-      name: 'title',
-      label: 'Title',
-      collection: 'questions',
-      valueField: 'questions_title',
-      searchFields: ['questions_title'],
-      widget: 'relation',
-      required: false,
-    },
-  },
-  {
-    name: 'resources_link',
-    label: '📚 Resource Title',
-    widget: 'list',
-    field: {
-      name: 'title',
-      label: 'Title',
-      collection: 'resources',
-      valueField: 'resources_title',
-      searchFields: ['resources_title'],
-      widget: 'relation',
-      required: false,
-    },
-  },
-];
-
-const buildTranslation = isoKey => ({
-  label: isoToLanguage(isoKey),
-  name: isoKey,
-  widget: 'object',
-  fields: langaugeInstance(false),
-});
-
-const removeEnglish = key => key !== 'eng';
-
-const translations = isoKeys => isoKeys
-  .filter(removeEnglish)
-  .map(buildTranslation);
-
-const createFolders = isoKeys => ({
+const createFolders = () => ({
   name: 'folders',
   label: '📁 Folders',
   folder: 'src/data/folders/',
@@ -63,15 +9,46 @@ const createFolders = isoKeys => ({
       name: 'type',
       label: 'Type',
       widget: 'hidden',
-      default: 'resourcefolders',
+      default: 'folders',
+    },
+    {
+      name: 'folders_title',
+      label: 'Folder Title',
+      widget: 'string',
     },
     {
       name: 'icon',
       label: 'Icon',
       widget: 'icon-selector',
     },
-    ...langaugeInstance(true),
-    ...translations(isoKeys),
+    {
+      name: 'questions_link',
+      label: '✏️ Questions Title',
+      widget: 'list',
+      field: {
+        name: 'title',
+        label: 'Title',
+        collection: 'questions',
+        valueField: 'questions_title',
+        searchFields: ['questions_title'],
+        widget: 'relation',
+        required: false,
+      },
+    },
+    {
+      name: 'resources_link',
+      label: '📚 Resource Title',
+      widget: 'list',
+      field: {
+        name: 'title',
+        label: 'Title',
+        collection: 'resources',
+        valueField: 'resources_title',
+        searchFields: ['resources_title'],
+        widget: 'relation',
+        required: false,
+      },
+    },
   ],
 });
 
