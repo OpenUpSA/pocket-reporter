be7fc6e8886273ab264b245cfc1368f044ff0b03
Schalk Venter <venterschalk@gmail.com>
2019-02-27 10:28:29 +0200

    Minor file changes

--- src/components/Icon/index.story.jsx
+++ src/components/Icon/index.story.jsx
@@ -4,10 +4,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Icon from './index';
+import { names as iconNames } from '../../config/icons.json';
 
 
 const basic = () => <Icon type="Public" />;
-
+const all = () => (
+  <div>
+    {iconNames.map(type => <Icon {...{ type }} key={type} />)}
+  </div>
+);
 
 storiesOf('component.Icon', module)
-  .add('Basic', basic);
+  .add('Single', basic)
+  .add('All', all);
