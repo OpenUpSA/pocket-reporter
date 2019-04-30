8e5a1b6f79296e4c9f8a972bc83c2e2f1ee26df2
Schalk Venter <venterschalk@gmail.com>
2019-04-30 16:29:36 +0200

    Changes service worker

--- node-build/services/node/createConfig/offlineConfig.js
+++ node-build/services/node/createConfig/offlineConfig.js
@@ -2,6 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var options = {
     importWorkboxFrom: "local",
+    globDirectory: '.',
+    globPatterns: ['public/**/*'],
+    globIgnores: ['public/admin/**/*', 'public/**/*.map'],
+    cacheId: "gatsby-plugin-offline",
+    skipWaiting: true,
+    clientsClaim: true,
 };
 var offlineConfig = {
     resolve: "gatsby-plugin-offline",
--- src/services/node/createConfig/offlineConfig.ts
+++ src/services/node/createConfig/offlineConfig.ts
@@ -1,11 +1,12 @@
 const options = {
   importWorkboxFrom: `local`,
   globDirectory: '.',
-  globPatterns: '*',
+  globPatterns: ['public/**/*'],
+  globIgnores: ['public/admin/**/*', 'public/**/*.map'],
   cacheId: `gatsby-plugin-offline`,
   skipWaiting: true,
   clientsClaim: true,
-}
+};
 
 const offlineConfig = {
   resolve: `gatsby-plugin-offline`,
