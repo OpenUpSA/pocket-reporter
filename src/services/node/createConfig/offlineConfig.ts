d54f455db56ae363b51b62a153d0d5cd9245b16d
Schalk Venter <venterschalk@gmail.com>
2019-04-30 15:50:45 +0200

    Fixes issue with service worker

--- src/services/node/createConfig/offlineConfig.ts
+++ src/services/node/createConfig/offlineConfig.ts
@@ -1,6 +1,17 @@
 const options = {
   importWorkboxFrom: `local`,
-};
+  globDirectory: '.',
+  globPatterns: '*',
+  cacheId: `gatsby-plugin-offline`,
+  runtimeCaching: [
+    {
+      urlPattern: /.*/,
+      handler: `cacheFirst`,
+    },
+  ],
+  skipWaiting: true,
+  clientsClaim: true,
+}
 
 const offlineConfig = {
   resolve: `gatsby-plugin-offline`,
